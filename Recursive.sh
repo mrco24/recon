2922cbce9cff7d1c1e45088dfac0016784bb1f9b
mrco24 <95030367+mrco24@users.noreply.github.com>
2022-05-19 15:52:34 +0600

    Update Recursive.sh

--- Recursive.sh
+++ Recursive.sh
@@ -6,8 +6,6 @@ Recursive(){
 for sub in $(cat $host);
 do
     subfinder -d $sub -all -silent | anew -q passive_recursive.txt
-    assetfinder --subs-only $sub | anew -q passive_recursive.txt
-    amass enum -passive -d $sub | anew -q passive_recursive.txt
     findomain --quiet -t $sub | anew -q assive_recursive.txt
     cat *.txt > R_allsub.txt
     cat R_allsub.txt | anew -q finalsub.txt
