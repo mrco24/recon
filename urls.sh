ed9f78c3ec0ac1f0660aeb39bb7b1406c01899cd
mrco24 <95030367+mrco24@users.noreply.github.com>
2022-09-04 12:03:17 +0600

    Update urls.sh

--- urls.sh
+++ urls.sh
@@ -5,7 +5,7 @@ host=$1
 urls(){
 for sub in $(cat $host);
 do
-curl "https://web.archive.org/cdx/search/cdx?url=$1/*&output=text&fl=original&collapse=urlkey" | grep "=" | tee urls.txt
+curl "https://web.archive.org/cdx/search/cdx?url=$sub/*&output=text&fl=original&collapse=urlkey" | grep "=" | tee urls.txt
 done
 }
 urls
