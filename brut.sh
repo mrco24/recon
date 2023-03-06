5c24b5d72e881cb920b1434e95a14a987ba63d55
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-03-07 00:19:32 +0530

    Update brut.sh

--- brut.sh
+++ brut.sh
@@ -4,11 +4,13 @@
 host=$1
 
 brut(){
-for domain in $(cat $host);
+for sub in $(cat $host);
 do
 
-puredns bruteforce /root/wordlist/SecLists/Discovery/DNS/dns-Jhaddix.txt $sub -r /root/wordlist/resolvers.txt && anew -q /root/recon/$domain/subdomain/good/puredns.txt
+puredns bruteforce /root/wordlist/SecLists/Discovery/DNS/dns-Jhaddix.txt $sub -r /root/wordlist/resolvers.txt | anew -q /root/recon/indriver.com/subdomain/good/puredns.txt
 
 done
 }
 brut
+
+
