80af11f06894daa5f18d23e0e49ce1fdb2df6daf
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-03-07 11:27:58 +0530

    Update brut.sh

--- brut.sh
+++ brut.sh
@@ -2,15 +2,25 @@
 #!/bin/bash
 
 host=$1
-
+resolver="/root/wordlist/resolvers.txt"
 brut(){
 for sub in $(cat $host);
 do
 
-puredns bruteforce /root/wordlist/SecLists/Discovery/DNS/dns-Jhaddix.txt $sub -r /root/wordlist/resolvers.txt | anew -q /root/recon/indriver.com/subdomain/good/puredns.txt
+puredns bruteforce /root/wordlist/SecLists/Discovery/DNS/1.txt $sub -r /root/wordlist/resolvers.txt | anew -q /root/recon/indriver.com/subdomain/good/puredns.txt
+cat *.txt > P_allsub.txt
+cat R_allsub.txt | anew -q puredns_bruteforce_sub.txt
 
 done
 }
 brut
 
-
+resolving_domains_2(){
+for domain in $(cat $host);
+do
+massdns -r $resolver -t A -o S -w /root/recon/$domain/subdomain/good/massdns.txt /root/recon/$domain/subdomain/good/Recursive_finalsub_all.txt
+cat /root/recon/$domain/subdomain/good/massdns.txt | sed 's/A.*//; s/CN.*// ; s/\..$//' | tee > /root/recon/$domain/subdomain/good/good_sub.txt
+#shuffledns -d /root/recon/$domain/subdomain/good/finalsub.txt -r /root/wordlist/resolvers.txt -o /root/recon/$domain/subdomain/good/good_sub.txt
+done
+}
+resolving_domains_2
