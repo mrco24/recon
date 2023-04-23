57c911ed1ba8e5354de8eeef351b02c72de60e0f
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-04-23 23:45:12 +0600

    Delete brup_crwl.sh

--- brup_crwl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set the Burp proxy address and port
-proxy="http://127.0.0.1:8080"
-url="/home/mobaxterm/urls.txt"
-# Loop over the URLs in the urls.txt file and send each URL through the Burp proxy using Curl
-while read url; do
-    curl -x "$proxy" -k "$url"
-done < urls.txt
