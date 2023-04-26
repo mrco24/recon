04a5e4d2f4749207d3dacf3eb0949974348f2eb9
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-04-26 14:32:36 +0600

    Update new_sub_scan.sh

--- new_sub_scan.sh
+++ new_sub_scan.sh
@@ -4,31 +4,31 @@ host=$1
 wordlist="/root/wordlist/all.txt"
 resolver="/root/wordlist/resolvers.txt"
 
-Special_subdomain(){
-for domain in $(cat $host);
-do
-cat /root/recon/$domain/subdomain/good/Gen_sub/active_subdomain.txt | anew /root/recon/$domain/Special_subdomain/Special_subdomain.txt 
-done
-}
-Special_subdomain
+#Special_subdomain(){
+#for domain in $(cat $host);
+#do
+#cat /root/recon/$domain/subdomain/good/Gen_sub/active_subdomain.txt | anew /root/recon/$domain/Special_subdomain/Special_subdomain.txt 
+#done
+#}
+#Special_subdomain
 
 Special_subdomain_scanner(){
 for domain in $(cat $host);
 do
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/my-nuclei-templates/cves/  -o /root/recon/$domain/Special_subdomain/scan/nuclei/my-cves.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/my-nuclei-templates/vulnerabilities/  -o /root/recon/$domain/Special_subdomain/scan/nuclei/my-vulnerabilities.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/my-nuclei-templates/technologies/  -o /root/recon/$domain/Special_subdomain/scan/nuclei/my-technologies.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/my-nuclei-templates/My-Nuclei-Templates/ -o /root/recon/$domain/Special_subdomain/scan/nuclei/My-Nuclei-Templates.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/my-nuclei-templates/Nuclei 1/ -o /root/recon/$domain/Special_subdomain/scan/nuclei/my-Nuclei.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t  nuclei -t /root/templates/my-nuclei-templates/workflows/ -o /root/recon/$domain/Special_subdomain/scan/nuclei/my-workflows.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/my-nuclei-templates/helpers/ -o /root/recon/$domain/Special_subdomain/scan/nuclei/my-helpers.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/my-nuclei-templates/idscan/ -o /root/recon/$domain/Special_subdomain/scan/nuclei/my-idscan.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/nuclei-templates/cves/ -o /root/recon/$domain/Special_subdomain/scan/new-nuclei/cve.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/nuclei-templates/vulnerabilities/  -o /root/recon/$domain/Special_subdomain/scan/new-nuclei/vulnerabilities.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/nuclei-templates/takeovers/ -o /root/recon/$domain/Special_subdomain/scan/new-nuclei/takover.txt -v
-cat /root/recon/$domain/Special_subdomain/Special_subdomain.txt | nuclei -t /root/templates/nuclei-templates/technologies/ -o /root/recon/$domain/Special_subdomain/scan/new-nuclei/technologies.txt -v
-jaeles scan -c 50 -s /root/templates/ghsec-jaeles-signatures -U /root/recon/$domain/Special_subdomain/Special_subdomain.txt -o /root/recon/$domain/Special_subdomain/scan/my-jaeles/ -v
-jaeles scan -c 50 -s /root/templates/jaeles-signatures -U /root/recon/$domain/Special_subdomain/Special_subdomain.txt -o /root/recon/$domain/Special_subdomain/scan/jaeles/ -v
+cat /root/sub.txt | nuclei -t /root/templates/my-nuclei-templates/cves/  -o /root/scan/my-cves.txt -v
+cat /root/sub.txt | nuclei -t /root/templates/my-nuclei-templates/vulnerabilities/  -o /root/scan/my-vulnerabilities.txt -v
+cat /root/sub.txt | nuclei -t /root/templates/my-nuclei-templates/technologies/  -o /root/scan/my-technologies.txt -v
+cat /root/sub.txt | nuclei -t /root/templates/my-nuclei-templates/My-Nuclei-Templates/ -o /root/scan/My-Nuclei-Templates.txt -v
+cat /root/sub.txt | nuclei -t /root/templates/my-nuclei-templates/Nuclei 1/ -o /root/scan/my-Nuclei.txt -v
+cat /root/sub.txt | nuclei -t  nuclei -t /root/templates/my-nuclei-templates/workflows/ -o /root/scan/my-workflows.txt -v
+cat /root/sub.txt | nuclei -t /root/templates/my-nuclei-templates/helpers/ -o /root/scan/my-helpers.txt -v
+cat /root/sub.txt | nuclei -t /root/templates/my-nuclei-templates/idscan/ -o /root/scan/my-idscan.txt -v
+cat /root/sub.txt | nuclei -t /root/templates/nuclei-templates/cves/ -o /root/scan/new/cve.txt -v
+cat /root/sub.txt | nuclei -t /root/templates/nuclei-templates/vulnerabilities/  -o /root/scan/new/vulnerabilities.txt -v
+cat /root/sub.txt | nuclei -t /root/templates/nuclei-templates/takeovers/ -o /root/scan/new/takover.txt -v
+cat /root/sub.txt | nuclei -t /root/templates/nuclei-templates/technologies/ -o /root/scan/new/technologies.txt -v
+jaeles scan -c 50 -s /root/templates/ghsec-jaeles-signatures -U /root/sub.txt -o /root/scan/my-jaeles/ -v
+jaeles scan -c 50 -s /root/templates/jaeles-signatures -U /root/sub.txt -o /root/scan/jaeles/ -v
 done
 }
 Special_subdomain_scanner
@@ -36,7 +36,15 @@ Special_subdomain_scanner
 Special_subdomain_Dir(){
 for domain in $(cat $host);
 do
-dirsearch -l /root/recon/$domain/Special_subdomain/Special_subdomain.txt > /root/recon/$domain/dri/Special_subdomain_Dri.txt
+dirsearch -l /root/sub.txt | tee -a /root/scan/Dri/domain_Dri.txt
 done
 }
 Special_subdomain_Dir
+
+Special_subdomain_Dir2(){
+for domain in $(cat $host);
+do
+dirsearch -l target.txt -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,sqlasp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip -i 200 | tee -a /root/scan/Dri/specail_dri.txt
+done
+}
+Special_subdomain_Dir2
