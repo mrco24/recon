484e4ef1f3cf8fda11b26a1bd9efe461d4957fd3
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-04-28 15:21:08 +0600

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -7,7 +7,7 @@ resolver="/root/wordlist/resolvers.txt"
 Gen_subdomain(){
 for domain in $(cat $host);
 do
-gotator -sub /root/recon/$domain/subdomain/good/Recursive_finalsub_all.txt -perm /root/wordlist/mrco24-wordlist/gen-sub-wordlist.txt -depth 3 > /root/recon/$domain/Subomain-Takeover/Gen_subdomain.txt
+gotator -sub /root/recon/$domain/subdomain/good/Recursive_finalsub_all.txt -perm /root/wordlist/mrco24-wordlist/gen-sub-wordlist.txt -depth 3 | tee -a /root/recon/$domain/Subomain-Takeover/Gen_subdomain.txt
 cat /root/recon/$domain/Subomain-Takeover/Gen_subdomain.txt | sort --unique | tee /root/recon/$domain/Subomain-Takeover/take_ge_subdomain.txt
 done
 }
@@ -52,7 +52,7 @@ http_probe_2
 Gen_subdomain(){
 for domain in $(cat $host);
 do
-gotator -sub /root/recon/$domain/subdomain/good/httprobe_subdomain.txt -perm /root/wordlist/mrco24-wordlist/gen-sub-wordlist.txt -depth 3 > /root/recon/$domain/subdomain/good/Gen_sub/Gen_subdomain.txt | cat /root/recon/$domain/subdomain/good/Gen_sub/Gen_subdomain.txt | httpx -o /root/recon/$domain/subdomain/good/Gen_sub/httpx_gen_sub.txt
+gotator -sub /root/recon/$domain/subdomain/good/httprobe_subdomain.txt -perm /root/wordlist/mrco24-wordlist/gen-sub-wordlist.txt -depth 3 | tee -a /root/recon/$domain/subdomain/good/Gen_sub/Gen_subdomain.txt | cat /root/recon/$domain/subdomain/good/Gen_sub/Gen_subdomain.txt | httpx -o /root/recon/$domain/subdomain/good/Gen_sub/httpx_gen_sub.txt
 cp /root/recon/$domain/subdomain/good/httprobe_subdomain.txt /root/recon/$domain/subdomain/good/Gen_sub
 cat /root/recon/$domain/subdomain/good/Gen_sub/*.txt > /root/recon/$domain/subdomain/good/Gen_sub/all-gen-sub.txt
 cat /root/recon/$domain/subdomain/good/Gen_sub/all-gen-sub.txt | sort --unique | tee/root/recon/$domain/subdomain/good/Gen_sub/active_subdomain.txt
