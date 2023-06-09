ae5e93701ffd299c6f05af1eaff92c1b7fb4f57a
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-06-09 16:54:03 +0600

    web_archive_urls.sh

--- web_archive_urls.sh
+++ web_archive_urls.sh
@@ -5,7 +5,7 @@ host=$1
 urls(){
 for sub in $(cat $host);
 do
-curl "https://web.archive.org/cdx/search/cdx?url=$sub/*&output=text&fl=original&collapse=urlkey" | grep "=" | tee -a web_archive_urls.txt
+curl "https://web.archive.org/cdx/search/cdx?url=$sub/*&output=text&fl=original&collapse=urlkey" | grep $sub | tee -a web_archive_urls.txt
 done
 }
 urls
