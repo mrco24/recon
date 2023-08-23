b7d219be5f8ee6d2d87e288e32ad19a38808538f
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-08-23 21:35:04 +0600

    brut.sh

--- brut.sh
+++ brut.sh
@@ -5,7 +5,7 @@ resolver="/root/wordlist/resolvers.txt"
 brut(){
 for sub in $(cat $host);
 do
-puredns bruteforce /root/wordlist/SecLists/Discovery/DNS/dns-Jhaddix.txt $sub -r /root/wordlist/resolvers.txt | tee -a hi.txt
+puredns bruteforce /root/wordlist/SecLists/Discovery/DNS/dns-Jhaddix.txt $sub -r /root/wordlist/resolvers.txt | tee -a puredns_sub.txt
 done
 }
 brut
