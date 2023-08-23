9b715b9735c02f2f71d4bfdd46ba354a9f707217
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-08-23 16:12:28 +0600

    brut.sh

--- brut.sh
+++ brut.sh
@@ -6,11 +6,7 @@ resolver="/root/wordlist/resolvers.txt"
 brut(){
 for sub in $(cat $host);
 do
-
-puredns bruteforce /root/wordlist/SecLists/Discovery/DNS/dns-Jhaddix.txt $sub -r /root/wordlist/resolvers.txt | anew -q /root/recon/indriver.com/subdomain/good/puredns.txt
-cat *.txt > P_allsub.txt
-cat P_allsub.txt | uniq -u > puredns_bruteforce_sub.txt
-
+puredns bruteforce /root/wordlist/SecLists/Discovery/DNS/dns-Jhaddix.txt $sub -r /root/wordlist/resolvers.txt | tee -a /root/recon/$domain/subdomain/good/fainal/sub_puredns.txt
 done
 }
 brut
