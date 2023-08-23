549f45deba7748f7b1a291fa64bcf122f09b2cd6
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-08-23 16:09:37 +0600

    brut.sh

--- brut.sh
+++ brut.sh
@@ -15,13 +15,3 @@ done
 }
 brut
 
-resolving_domains_2(){
-for sub in $(cat $host);
-do
-
-massdns -r $resolver -t A -o S -w /root/recon/indriver.com/subdomain/good/massdns_2.txt /root/recon/indriver.com/subdomain/good/puredns_bruteforce_sub.txt
-cat /root/recon/indriver.com/subdomain/good/massdns_2.txt | sed 's/A.*//; s/CN.*// ; s/\..$//' | tee > /root/recon/indriver.com/subdomain/good/puredns_good_sub.txt
-#shuffledns -d /root/recon/$domain/subdomain/good/finalsub.txt -r /root/wordlist/resolvers.txt -o /root/recon/$domain/subdomain/good/puredns_good_sub.txt
-done
-}
-resolving_domains_2
